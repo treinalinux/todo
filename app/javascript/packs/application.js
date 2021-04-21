736eb1596246d1cc2e9f1f60eec2146d35ad8e40
Alan da Silva Alves <linuxfama@gmail.com>
2021-04-20 23:36:09 -0300

    Add and config bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,9 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap"
+
+import "../src/application.css"
+
 Rails.start()
 ActiveStorage.start()
